318e9e2cc4a290fd7dbbc31928d656f94daef187
Martín <magadiflo@gmail.com>
2022-05-16 20:07:11 -0500

    Creando el proyecto FrontEnd Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ChatComponent } from './chat/chat.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'chat', pathMatch: 'full' },
+  { path: 'chat', component: ChatComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
